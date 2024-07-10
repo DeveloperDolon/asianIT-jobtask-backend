fbebd3d81ae03156d17271da36cefc8247246f66
Dolon Chandra Roy <dolonr718@gmail.com>
2024-07-10 13:15:38 +0600

    user login api implemented commit

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,3 +1,5 @@
+import httpStatus from 'http-status';
+import config from '../../config';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { UserServices } from './user.service';
@@ -17,7 +19,21 @@ const createUser = catchAsync(async (req, res) => {
 });
 
 const loginUser = catchAsync(async (req, res) => {
-  const result = await UserServices.loginUserFromDB(req.body);
+  const { refreshToken, accessToken } = await UserServices.loginUserFromDB(
+    req.body,
+  );
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User logged in successfully!',
+    data: { accessToken },
+  });
 });
 
 export const UserController = { createUser, loginUser };
